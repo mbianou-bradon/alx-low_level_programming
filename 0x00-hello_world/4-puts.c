ca4e298032837b16253ef1aeba6c0311c0c3a6d8
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-02-16 08:05:13 -0800

    Advanced task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
+
 /**
+ * main - Entry point
  *
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
- *main- Entry point
-
- * Desctiption: prints "Programming is like building a multilingual puzzle"
- *
- * return: Always 0 (Sucess)
- *
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
- *Return: Always 0 (Success)
- *
- *
+ * Return: Always 0 (Success)
  */
-
-
-
-
-int main (void)
+int main(void)
 {
-
-
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
