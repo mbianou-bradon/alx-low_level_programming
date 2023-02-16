119c5229876f960ce5b7baf48cdd993f91ca440b
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-02-16 07:40:56 -0800

    Updated task with descriptions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+/**
+ *
+ *
+ *main- Entry point
+
+ * Desctiption: prints "Programming is like building a multilingual puzzle"
+ *
+ * return: Always 0 (Sucess)
+ *
+ */
+
 
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ *main - print the size of various types
+ *
+ *Description: prints the size of various types on the computer it is compiled and run on
+ *
+ *Return 0 (Success)
+ *
+ *
+ */
+
 int main(void)
 {
 	printf("Size of a char: %d bytes(s)\n", (int) sizeof(char));
