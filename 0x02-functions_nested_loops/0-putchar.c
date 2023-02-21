de5bda84dadc1a291bff552d3f37a4484e0291d0
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-02-21 06:07:05 -0800

    Adjusted task0 and done advanced task14

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-	char text[10] = "_putchar";
+	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
 	}
 	_putchar('\n');
 
 	return (0);
-}
+
