80e219eb6572e91f36936417a5352cfa5c3aad0e
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-02-21 02:38:23 -0800

    Initializing and completed task1 to task 4

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "putchar.c"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int cycle = 0;
+	char letter = 'a';
+
+	while (cycle < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		cycle++;
+	}
+}
