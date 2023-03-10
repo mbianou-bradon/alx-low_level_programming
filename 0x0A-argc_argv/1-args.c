7a0b375dc6743ba02252cbd1f57edaa3842a370c
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-03-10 12:10:14 -0800

    Advanced Task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the number of args 
+  * main - Prints the number of args
   * @argc: argument count
   * @argv: argument vector
   *
   * Return: Always zero
   */
 int main(int argc, char *argv[])
-{ 
+{
 	(void) argv;
 	printf("%d\n", argc - 1);
 	return (0);
