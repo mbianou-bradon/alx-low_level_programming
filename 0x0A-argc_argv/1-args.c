00f3943b4a384c18cc3046334789b5bda57135ba
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-03-10 12:05:11 -0800

    Adjusted task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints the number of args 
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: Always zero
- */
+  * main - Prints the number of args 
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 { 
 	(void) argv;
