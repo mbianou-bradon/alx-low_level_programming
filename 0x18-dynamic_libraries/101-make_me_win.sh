d253532a37a9aafb313ae7c572ac8a47d89d3e5e
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-06-23 18:18:43 -0700

    Adjusted 101-make_me_win again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp  https://raw.githubusercontent.com/mbianou-bradon/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101-makemewin.so
+wget -P /tmp  https://github.com/mbianou-bradon/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101-makemewin.so
 export LD_PRELOAD=/tmp/lib101-makemewin.so
